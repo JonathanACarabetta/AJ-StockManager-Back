ae3a6ac02c4f6bea1cbb19c71bf655e76aae35b3
Jonathan <158221181+JonathanACarabetta@users.noreply.github.com>
2024-10-01 17:28:24 -0300

    ADD update cost and Price Endpoint with an interceptor

---
+++ src/Interceptors/updateProductPriceAndCost.interceptor.ts
@@ -0,0 +1,23 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { EditPriceCost } from "../Product/dto/editPriceCost";
+
+@Injectable()
+export class updateProductPriceAndCostInterceptor implements NestInterceptor{
+    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+        const request = context.switchToHttp().getRequest();
+        const products = request.body;
+        const filteredProducts: EditPriceCost[] = products.map(product =>{
+            const price = product.price ? parseFloat(product.price):null;
+            const cost = product.cost ? parseFloat(product.cost):null;
+            const editProduct :EditPriceCost= {
+                id: product.id,
+                price: price,
+                cost: cost
+            };
+            return editProduct;
+        })
+        request.body = filteredProducts;
+        return next.handle();
+    }
+}
